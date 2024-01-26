04bbd7aa7e0bb7438f71f3461e58e4ee3b895540
rdso1984 <rdso1984@gmail.com>
2024-01-26 01:06:04 -0300

    Criado as acoes da api

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -6,12 +6,17 @@ import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controle {
 
   @Autowired
@@ -23,8 +28,18 @@ public class Controle {
   }
 
   @GetMapping("/")
-  public String teste() {
-    return "Hello World!";
+  public Iterable<Cliente> selecionar() {
+    return acao.findAll();
+  }
+
+  @PutMapping("/")
+  public Cliente editar(@RequestBody Cliente c) {
+    return acao.save(c); // metodo save faz tanto alteração como inclusão
+  }
+
+  @DeleteMapping("/{codigo}")
+  public void remover(@PathVariable long codigo) {
+    acao.deleteById(codigo);
   }
 
 }
